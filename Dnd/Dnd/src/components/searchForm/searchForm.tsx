a7a6e008c1be015d1e3ca17140b3029c4507ccc6
searsear5 <supanutbbp@gmail.com>
2024-03-20 22:40:50 +0700

    UI form with fillter

--- Dnd/Dnd/src/components/searchForm/searchForm.tsx
+++ Dnd/Dnd/src/components/searchForm/searchForm.tsx
@@ -1,13 +1,37 @@
 import React from 'react'
+import { typesList, sizeList } from '@/utils/optionList'
 
 const searchForm = () => {
     return (
-        <div className='grid grid-cols-3'>
-            <div>11</div>
-            <div>22</div>
-            <div>33</div>
+        <form className='grid grid-cols-3 gap-x-[50px] mx-8'>
+            <div>
 
-        </div>
+                <label htmlFor="type" className="block mb-2 text-mb font-medium text-white dark:text-white">type</label>
+                <select id="type" className="bg-[#253641] border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+
+                    {typesList.map((item, index) => {
+                        return <option key={`type-key-${index}`} value={item}>{item}</option>
+                    })}
+                </select>
+
+            </div>
+            <div>
+                <label htmlFor="size" className="block mb-2 text-mb font-medium text-white dark:text-white">size</label>
+                <select id="size" className="bg-[#253641] border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+
+                    {sizeList.map((item, index) => {
+                        return <option key={`size-key-${index}`} value={item}>{item}</option>
+                    })}
+                </select>
+            </div>
+            <div>
+                <label htmlFor="search" className="block mb-2 text-mb font-medium text-white dark:text-white">search</label>
+                <input id="search" className="bg-[#253641] border border-gray-300 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+
+                />
+            </div>
+
+        </form>
     )
 }
 
---
+++ Dnd/Dnd/src/utils/optionList.ts
@@ -0,0 +1,30 @@
+export const typesList = [
+    'all types',
+    'aberration',
+    'beast',
+    'celestial',
+    'construct',
+    'dragon',
+    'elemental',
+    'fey',
+    'fiend',
+    'giant',
+    'humanoid',
+    'monstrosity',
+    'ooze',
+    'plant',
+    'undead',
+    'tags',
+
+]
+
+export const sizeList = [
+    'all size',
+    'Tiny',
+    'Small',
+    'Medium',
+    'Large',
+    'Huge',
+    'Gargantuan',
+
+]
